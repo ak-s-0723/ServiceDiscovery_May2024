0a1ef6075ff7ba0b49c48c4694c720443ffa004f
khannaanurag1 <khannaanurag1@gmail.com>
2024-07-20 17:57:49 +0530

    Created Project

---
+++ src/main/java/org/example/servicediscovery_may2024/ServiceDiscoveryMay2024Application.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_may2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceDiscoveryMay2024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceDiscoveryMay2024Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/servicediscovery_may2024/ServiceDiscoveryMay2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.servicediscovery_may2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryMay2024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
