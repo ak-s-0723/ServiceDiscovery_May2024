43772079e383e6a9633712b55dacacb3cc0fa1fe
khannaanurag1 <khannaanurag1@gmail.com>
2024-07-20 18:03:50 +0530

    Testing

--- src/main/java/org/example/servicediscovery_may2024/ServiceDiscoveryMay2024Application.java
+++ src/main/java/org/example/servicediscovery_may2024/ServiceDiscoveryMay2024Application.java
@@ -2,7 +2,9 @@ package org.example.servicediscovery_may2024;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class ServiceDiscoveryMay2024Application {
 
